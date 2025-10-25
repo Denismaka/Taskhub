cc2abd3be5b12392317aa0df5656460194c7c05f
Denismaka <makadenis370@gmail.com>
2025-10-25 20:12:02 +0100

    Configuration finale pour GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Taskhub/', // Nom du repository GitHub : Taskhub
+  base: '/Taskhub/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  publicDir: 'public',
 })
