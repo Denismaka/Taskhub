234edc5ba156d1d362d8963e82a1eeb1d99e8bcf
Denismaka <makadenis370@gmail.com>
2025-10-25 20:32:25 +0100

    correction

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Taskhub/',
+  base: '/Taskhub/', 
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    emptyOutDir: true, 
   },
   publicDir: 'public',
 })
